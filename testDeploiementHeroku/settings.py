b0e96d4776a4d836319c17321951c265738a2b28
Oungaya <alypmat@gmail.com>
2018-05-09 13:37:35 +0200

    modification du HTML biatch

--- testDeploiementHeroku/settings.py
+++ testDeploiementHeroku/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q@$ad&9+0rzxkh=7)&oxm6eyz4f_)8-7m@3oe68wq7^bdu^x4+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oungaya.pythonanywhere.com', 'localhost']
 
 
 # Application definition
--- testDeploiementHeroku/wsgi.py
+++ testDeploiementHeroku/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for testDeploiementHeroku project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/Oungaya/testDeploiement'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'testDeploiementHeroku.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testDeploiementHeroku.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
